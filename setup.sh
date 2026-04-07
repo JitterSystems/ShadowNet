c81fcb6c806c2256d7623686a29090834b2b39e4
GothamBlvck <gothamblvck@gmail.com>
2026-04-07 09:16:16 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-YELLOW='\033[1;33m'
 NC='\033[0m'
 
 if [[ $EUID -ne 0 ]]; then
@@ -11,7 +10,7 @@ if [[ $EUID -ne 0 ]]; then
 	
 	echo -e "${GREEN}[+] Installing dependencies...${NC}"
 	apt-get update -y
-	DEBIAN_FRONTEND=noninteractive apt-get install -y tor iptables-persistent iproute2 curl tlsdate macchanger haveged net-tools
+	DEBIAN_FRONTEND=noninteractive apt-get install -y tor iptables-persistent iproute2 curl tlsdate macchanger haveged net-tools dnsutils
 	
 	chmod +x shadow.sh
 	echo -e "${GREEN}[V] Setup Complete.${NC}"
